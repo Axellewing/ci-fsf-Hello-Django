fbfea30afb3b4252a47517dca8645be1ec619c5b
Axel Lewing <axel_lewing@hotmail.com>
2024-01-10 00:42:36 +0000

    fixing NameError / typo in the tests.views as well as deleting the tests.py for more structual tests files

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import item
+from .models import Item
 
 
 class TestViews(TestCase):
@@ -10,7 +10,7 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
     def test_get_edit_item_page(self):
-        item = item.objects.create(name= 'Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/edit_item.html')
@@ -25,17 +25,17 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
 
     def test_can_delete_item(self):  
-        item = item.objects.create(name= 'Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
-        exsisting_item = item.object.filter(id=item.id)
+        exsisting_item = Item.objects.filter(id=item.id)
         self.assertEqual(len(exsisting_item), 0)
     
     def test_can_toggle_item(self):  
-        item = item.objects.create(name= 'Test Todo Item', done=True)
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')                 
-        updated_item = item.object.get(id=item.id)
+        updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)    
 
 
